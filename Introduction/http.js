6937ac2e6ae19277523b485d024b4cf82c3ae4dd
Gokberk Yildirim <gokberky1996@gmail.com>
2021-02-21 01:08:58 +0300

    Express learning part

--- Introduction/http.js
+++ Introduction/http.js
@@ -1,78 +1,95 @@
 const http = require("http")
-const fs = require("fs")
+const routes = require("./routes")
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(routes)
 
-    // if (req.url === "/test") {
+// const server = http.createServer((req, res) => {
 
-    //     // res.setHeader("Content-Type", "text/plain")
-    //     res.setHeader("Content-Type", "application/json")
-    //     res.statusCode = 200
-    //     res.statusMessage = "OK"
+//     if (req.url === "/test") {
 
-    //     res.write(JSON.stringify({ name: "Gokberk", age: 25, hobby: "programming", isAdmin: true }))
-    //     res.end()
-    // }
+//         // res.setHeader("Content-Type", "text/plain")
+//         res.setHeader("Content-Type", "application/json")
+//         res.statusCode = 200
+//         res.statusMessage = "OK"
 
-    // if (req.url === "/html") {
-    //     res.setHeader("Content-Type", "text/html")
-    //     res.statusCode = 200
-    //     res.statusMessage = "OK"
+//         res.write(JSON.stringify({ name: "Gokberk", age: 25, hobby: "programming", isAdmin: true }))
+//         res.end()
+//     }
 
-    //     res.write("<html>")
-    //     res.write("<head><title>Javascript</title></head>")
-    //     res.write("<body><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis dolorum dolor error, fuga aperiam necessitatibus, dignissimos accusamus odit aque ullam fugiat beatae modi voluptatibus corrupti recusandae libero facere ipsa tempore?</p></body>")
-    //     res.write("</html>")
-    //     res.end()
-    // }
+//     if (req.url === "/html") {
+//         res.setHeader("Content-Type", "text/html")
+//         res.statusCode = 200
+//         res.statusMessage = "OK"
 
-    // if (req.url === "/") {
-    //     res.write("Hello World")
-    //     res.end()
-    // }
+//         res.write("<html>")
+//         res.write("<head><title>Javascript</title></head>")
+//         res.write("<body><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis dolorum dolor error, fuga aperiam necessitatibus, dignissimos accusamus odit aque ullam fugiat beatae modi voluptatibus corrupti recusandae libero facere ipsa tempore?</p></body>")
+//         res.write("</html>")
+//         res.end()
+//     }
 
-    // if (req.url === "/api/products") {
-    //     res.write("products list")
-    //     res.end()
-    // }
+//     if (req.url === "/") {
+//         res.write("Hello World")
+//         res.end()
+//     }
 
-    // fs.readFile("Introduction/index.html", function (err, file) {
-    //     if (err) {
-    //         res.setHeader("Content-Type", "text/plain")
-    //         res.statusCode = 404
-    //         res.statusMessage = "Not Found"
-    //         res.end("Not Found")
-    //     } else {
-    //         res.setHeader("Content-Type", "text/html")
-    //         res.statusCode = 200
-    //         res.statusMessage = "OK"
-    //         res.end(file)
-    //     }
-    // })
+//     if (req.url === "/api/products") {
+//         res.write("products list")
+//         res.end()
+//     }
 
-    if (req.url === "/") {
-        res.write(`<html>
-        <head>
-            <title>Javascript</title>
-        </head>
-        <body>
-            <form action="/log" method="POST">
-            <input type="text" name="message">
-            <button type="submit">Save</button>
-            </form>
-        </body>
-        </html>
-        `)
-        return res.end()
-    }
+//     fs.readFile("Introduction/index.html", function (err, file) {
+//         if (err) {
+//             res.setHeader("Content-Type", "text/plain")
+//             res.statusCode = 404
+//             res.statusMessage = "Not Found"
+//             res.end("Not Found")
+//         } else {
+//             res.setHeader("Content-Type", "text/html")
+//             res.statusCode = 200
+//             res.statusMessage = "OK"
+//             res.end(file)
+//         }
+//     })
 
-    if (req.url === "/log" && req.method === "POST") {
-        fs.writeFileSync("Introduction/log.txt", "deneme")
-        res.statusCode = 302
-        res.setHeader("Location", "/")
-        return res.end()
-    }
-})
+//     res.setHeader("Content-Type", "text/html")
+//     if (req.url === "/") {
+//         res.write(`<html>
+//         <head>
+//             <title>Javascript</title>
+//         </head>
+//         <body>
+//             <form action="/log" method="POST">
+//             <input type="text" name="message">
+//             <button type="submit">Save</button>
+//             </form>
+//         </body>
+//         </html>
+//         `)
+//         return res.end()
+//     }
+
+//     if (req.url === "/log" && req.method === "POST") {
+
+//         // body parser
+//         const body = []
+//         req.on("data", function (chunk) {
+//             body.push(chunk)
+//             console.log(chunk)
+//         })
+
+//         req.on("end", () => {
+//             const bodyParsed = Buffer.concat(body).toString()
+//             const message = bodyParsed.split("=")[1]
+//             console.log(qs.parse(bodyParsed))
+//             fs.appendFileSync("Introduction/log.txt", message)
+//         })
+
+//         res.statusCode = 302
+//         res.setHeader("Location", "/")
+//         return res.end()
+//     }
+// })
 
 server.listen(3000)
 
--- README.md
+++ README.md
@@ -5,15 +5,28 @@ Learning series about nodejs
 ### Table of Contents
 
 - [Introduction](#introduction)
-- [Supplementary Code Snippets](#supplementary-code-snippets)
+- [Npm](#npm)
+- [Express](#express)
 
 ## Introduction
 
 > All the necessary files is in Introduction directory \
-> `node_modules` including `fs, path, url, events, http` mentioned in this section \
+> `node_modules` including `fs, path, url, events, http, querystring` mentioned in this section \
 > Usage of some modules explained with a simple example
 
-## Supplementary Code Snippets
+## Npm
 
+> `^major.minor.patch` npm packages versioning, `^` trackes the change of minor or patch part not major part so if any major update is available on a package, it is not installed the project\
+> `npm i [package_name] --save-dev` npm packages will be installed as devDependencies. It is a local package but it is only for development purposes \
+> `npm -v` Gives the installed npm version \
 > `npm list -g --depth 0` Lists global npm packages on the machine \
-> `npm i -g [package_name]` Install npm package into system globally
+> `npm i -g [package_name]` Install npm package into system globally \
+> `npm config ls -l | grep prefix` Global npm packages installed this directory
+> `npm init` Created packages.json \
+> `npm init -y` Created packages.json with default settings \
+> `npm i` If there is no node_modules it installs all the dependencies \
+> `npm i [package_name@version_number]` Install npm packages with specific version \
+> `npm uninstall [package_name]` Uninstall npm packages \
+> If define a npm script name reserved by npm, this command canbe executed with `npm run [command_name]`
+
+## Express
